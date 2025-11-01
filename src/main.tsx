9c6368413dc2d02505412d2a6afd53a775357ec6
Avyvka <lamonov.yura@gmail.com>
2025-11-01 21:33:12 +0300

    feat: Add dark-theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { KcPage } from "./kc.gen";
 import "./index.css";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
 
 // The following block can be uncommented to test a specific page with `yarn dev`
 // Don't forget to comment back or your bundle size will increase
@@ -18,10 +19,12 @@ if (import.meta.env.DEV) {
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        {!window.kcContext ? (
-            <h1>No Keycloak Context</h1>
-        ) : (
-            <KcPage kcContext={window.kcContext} />
-        )}
+        <ThemeProvider defaultTheme="dark" storageKey="theme">
+            {!window.kcContext ? (
+                <h1>No Keycloak Context</h1>
+            ) : (
+                <KcPage kcContext={window.kcContext} />
+            )}
+        </ThemeProvider>
     </StrictMode>
 );
